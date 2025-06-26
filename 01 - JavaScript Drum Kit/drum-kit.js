dc15e18f483d3eec7fb8ed075292c4e6d5f50e34
K3A101 <alexanderkeisha9@gmail.com>
2025-06-26 19:51:17 +0200

    Day 1 completion

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -1,9 +1,18 @@
-document.addEventListener('keydown', (e)=> {
-   //  const audio = document.querySelector(`audio[data-key="${e.code}]"`);
-    const audio = document.querySelector(`audio[data-key='${e.key}']`);
-   if(!audio) return;
-   audio.currentTime = 0;
-    audio.play() 
-    console.log(audio);
-    
-  });
+document.addEventListener('keydown', playSound);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key='${e.key}']`);
+  const key = document.querySelector(`.key[data-key='${e.key}']`);
+  if (!audio) return;
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
