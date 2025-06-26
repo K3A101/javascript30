5e4adec12e578aaa016b77b47ed740bdf2b199d4
K3A101 <alexanderkeisha9@gmail.com>
2025-06-26 17:09:06 +0200

    Day 1

---
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -0,0 +1,8 @@
+document.addEventListener('keydown', (e)=> {
+   //  const audio = document.querySelector(`audio[data-key="${e.code}]"`);
+    const audio = document.querySelector(`audio[data-key='${e.key}']`);
+   if(!audio) return;
+    audio.play() 
+    console.log(audio);
+    
+  });
