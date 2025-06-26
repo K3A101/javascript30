6180821b37a8cd49c99e13ec88b93dbb54911a89
K3A101 <alexanderkeisha9@gmail.com>
2025-06-26 19:27:20 +0200

    Adding eslint

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -2,6 +2,7 @@ document.addEventListener('keydown', (e)=> {
    //  const audio = document.querySelector(`audio[data-key="${e.code}]"`);
     const audio = document.querySelector(`audio[data-key='${e.key}']`);
    if(!audio) return;
+   audio.currentTime = 0;
     audio.play() 
     console.log(audio);
     
