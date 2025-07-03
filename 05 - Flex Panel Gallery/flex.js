a682ffcc575bb6aa8aa2899bf33fefa41dd34164
K3A101 <alexanderkeisha9@gmail.com>
2025-07-03 09:58:08 +0200

    Day 5: Excersises

--- 05 - Flex Panel Gallery/flex.js
+++ 05 - Flex Panel Gallery/flex.js
@@ -1,8 +1,27 @@
 const panels = document.querySelectorAll('.panel');
 
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+// function toggleActive() {
+//   if (this.classList.contains('open')) {
+//     this.classList.add('open-active');
+//   } else {
+//     this.classList.remove('open-active');
+//   }
+// }
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    // Includes the word flex for browser support
+    this.classList.toggle('open-active');
+  }
+}
+
 panels.forEach((panel) => {
-  panel.addEventListener('click', () => {
-    console.log('This hovering');
-    panel.classList.toggle('open', 'open-active');
-  });
+  panel.addEventListener('click', toggleOpen);
+});
+
+panels.forEach((panel) => {
+  panel.addEventListener('transitionend', toggleActive);
 });
